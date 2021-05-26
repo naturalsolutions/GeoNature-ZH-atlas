d36231fc32efec7ce65aa607b90f037e48c70a8f
Javier Blanco <javier_blanco@natural-solutions.eu>
2021-05-26 09:15:48 +0200

    fix: center france coords

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -11,8 +11,8 @@ const Map: FC<MapProps> = ({ geojson }) => {
   return (
     <MapContainer
       style={{ width: '100%', height: '100%' }}
-      center={[51.505, -0.09]}
-      zoom={13}
+      center={[47.8, 2.6]}
+      zoom={6}
     >
       <LayersControl position="topright">
         <LayersControl.BaseLayer checked name="OpenStreetMap">
@@ -21,13 +21,13 @@ const Map: FC<MapProps> = ({ geojson }) => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
         </LayersControl.BaseLayer>
-        <LayersControl.BaseLayer checked name="OpenTopoMap">
+        <LayersControl.BaseLayer name="OpenTopoMap">
           <TileLayer
             attribution="© OpenTopoMap"
             url="https://a.tile.opentopomap.org/{z}/{x}/{y}.png"
           />
         </LayersControl.BaseLayer>
-        <LayersControl.BaseLayer checked name="GoogleSatellite">
+        <LayersControl.BaseLayer name="GoogleSatellite">
           <TileLayer
             attribution="© GoogleMap"
             url="https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
