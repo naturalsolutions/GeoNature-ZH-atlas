4b1f992729dbddec59ff7e5f89f09aa32ecf2b2e
Javier Blanco <javier_blanco@natural-solutions.eu>
2021-06-28 12:08:44 +0200

    ci: test build image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const yaml = require('js-yaml')
 const fs = require('fs')
 
+const geojson = {
+  features: [],
+}
+
 let publicRuntimeConfig = {
   pages: {
     home: {
@@ -22,20 +26,24 @@ try {
   console.log(e)
 }
 
+const routes = geojson.features.reduce((acc, value) => {
+  acc[`/map/${value.properties.slug}`] = {
+    page: '/map/[slug]',
+    query: {
+      slug: value.properties.slug,
+    },
+  }
+
+  return acc
+}, {})
+
 module.exports = {
   publicRuntimeConfig,
   exportPathMap: async function () {
     return {
       '/': { page: '/' },
       '/map': { page: '/map' },
-      '/map/zh-exemple-1': {
-        page: '/map/[slug]',
-        query: { slug: 'zh-exemple-1' },
-      },
-      '/map/zh-exemple-2': {
-        page: '/map/[slug]',
-        query: { slug: 'zh-exemple-2' },
-      },
+      ...routes,
     }
   },
 }
