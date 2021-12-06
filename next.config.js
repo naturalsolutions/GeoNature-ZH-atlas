5e02831ecd9ba322f3b5c8b0f176a8eb761aaea9
Javier Blanco <javier_blanco@natural-solutions.eu>
2021-12-06 13:48:58 +0100

    docs: creation docs structure

--- next.config.js
+++ next.config.js
@@ -2,36 +2,50 @@ const yaml = require('js-yaml')
 const fs = require('fs')
 const axios = require('axios')
 
-if (!process.env.GEOJSON_URL) {
-  throw new Error('GEOJSON_URL is mandatory')
-}
-
 let publicRuntimeConfig = {
   pages: {
     home: {
-      title: '',
+      title: 'GeoNature Zone Humides · Atlas',
       heroText: '',
+      text: [],
+      images: [],
     },
   },
   layout: {
     links: [{ title: '', href: '' }],
+    header: {
+      logo: {
+        src: '',
+        alt: '',
+      },
+    },
     footer: {
       images: [{ name: '', image: '' }],
+      links: [],
+      legal: [],
     },
   },
 }
 
-try {
+if (fs.existsSync('./data/config.yml')) {
   publicRuntimeConfig = yaml.load(fs.readFileSync('./data/config.yml', 'utf8'))
-} catch (e) {
-  console.log(e)
 }
 
 module.exports = {
   publicRuntimeConfig,
   trailingSlash: true,
   exportPathMap: async function () {
-    const { data: geojson } = await axios.get(process.env.GEOJSON_URL)
+    const geojsonData = fs.readFileSync('./public/geonature.geojson')
+    let geojson = JSON.parse(geojsonData)
+
+    if (process.env.GEOJSON_URL) {
+      const { response, data } = await axios.get(process.env.GEOJSON_URL)
+
+      if (response === 200) {
+        geojson = data
+      }
+    }
+
     const routes = geojson.features.reduce((acc, value) => {
       acc[`/map/${value.properties.slug}`] = {
         page: '/map/[slug]',
